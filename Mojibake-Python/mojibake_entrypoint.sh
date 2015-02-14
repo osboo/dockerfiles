e9828c2d6caf440030010e5df474503e2ff39df6
ardinor <jordan@defestri.org>
2015-02-14 20:10:30 +1000

    More tinkering with mojibake

--- Mojibake-Python/mojibake_entrypoint.sh
+++ Mojibake-Python/mojibake_entrypoint.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+set -e
+
+if [ -z "$ADMIN_USER" -o -z "$ADMIN_PASS" ]; then
+    echo >&2 'error: ADMIN_USER and/or ADMIN_PASS is not set'
+    echo >&2 '  Did you forget to add -e ADMIN_USER=<name> ADMIN_PASS=<pass>'
+    exit 1
+fi
+
+echo "Running mojibake setup..."
+echo $ADMIN_USER
+echo $ADMIN_PASS
+
+python3.4 /opt/mojibake/apps/mojibake/setup.py "$ADMIN_USER" "$ADMIN_PASS"
 
 start_mojibake_as_user()
 {
